58a7caa78f425486f0de49a7b88953c229a040c0
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-29 19:34:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  return num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  return num1-num2
 end
 
 def division(num1, num2)
-
+  return num1/num2
 end
 
 def multiplication(num1, num2)
+  return num1 * num2
 
 end
 
 def modulo(num1, num2)
+  return num1 % num2
 
 end
 
-def square_root(num)
-
+def square_root(num=81)
+  return Math.squrt{num}
 end
+
+square_root(9)
+
