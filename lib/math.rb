3d35591c8f28457573dd1ac7e117c4f552c0f1f6
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-29 20:04:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,21 +10,26 @@ def subtraction(num1, num2)
   return num1-num2
 end
 
+return subtraction(5,4)
+
 def division(num1, num2)
   div_sum = num1/num2
   div_sum
 end
 
+division(4,2)
+
 
 def multiplication(num1, num2)
   return num1 * num2
-
 end
+multiplication(5,4)
 
 def modulo(num1, num2)
   return num1 % num2
-
 end
+
+modulo(6,5)
 
 def square_root(num)
   
