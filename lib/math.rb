220a878819fd7bcffb237c95b49fc6d66361161d
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-29 20:06:34 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,41 @@
-def addition(num1, num2)
 
+def addition(num1, num2)
+ return num1+ num2
 end
+addition(2,3)
+
+
 
 def subtraction(num1, num2)
-
+  return num1-num2
 end
+
+subtraction(5,4)
 
 def division(num1, num2)
-
+  div_sum = num1/num2
+  div_sum
 end
+
+division(4,2)
+
 
 def multiplication(num1, num2)
-
+  return num1 * num2
 end
+
+multiplication(5,4)
 
 def modulo(num1, num2)
-
+  return num1 % num2
 end
+
+modulo(6,5)
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
+
+square_root(81)
+
+
